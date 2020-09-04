bc724c7dfbc86e28c89754f664950c9e40c3592b
sanjh-dev <sanjhjaiswal2907>
2020-09-04 14:55:36 +0530

    WH-7865 Commit next 2

--- src/main/java/com/example/demo/model/WhUserType.java
+++ src/main/java/com/example/demo/model/WhUserType.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 public class WhUserType {
 	public static void main(String[] args) {
 		Logger log=LoggerFactory.getLogger(WhUserType.class);
+		System.out.println("hi");
 	}
 	
  }
