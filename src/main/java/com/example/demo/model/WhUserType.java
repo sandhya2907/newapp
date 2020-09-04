6c43d55ec3528814a853cc906c21d7ccf4943659
sanjh-dev <sanjhjaiswal2907>
2020-09-04 14:46:33 +0530

    NEw file commit

--- src/main/java/com/example/demo/model/WhUserType.java
+++ src/main/java/com/example/demo/model/WhUserType.java
@@ -1,5 +1,9 @@
 package com.example.demo.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class WhUserType {
+	private static Logger log=LoggerFactory.getLogger(WhUserType.class);
 
 }
