3cc1b5720e2111a5f95d737e02ede973b15f0df6
sanjh-dev <sanjhjaiswal2907>
2020-09-23 13:12:51 +0530

    no modified

--- src/main/java/com/example/demo/SbCurd1Application.java
+++ src/main/java/com/example/demo/SbCurd1Application.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SbCurd1Application {
 
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(SbCurd1Application.class, args);
 		
 	}
